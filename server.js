7a7cf98944cd410df105abc92b3cc4b5094bdd0c
Rakesh <varanasirakesh22@gmail.com>
2020-09-20 18:11:30 +0530

    initial commmit

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51HQuLnH8oF9FeaA0Gk4DVMWdFvxJ8rSjoJ8YRsP7Tk7dCNeVzEu4huoQsw0I8XJNML07HRh6e7iRx6nh9QCwZc3A00Vitw5Ojo"
+);
+// Config dotev
+require("dotenv").config({
+  path: "./config/config.env",
+});
+
+const app = express();
+
+// Connect to database
+connectDB();
+
+// body parser
+app.use(bodyParser.json());
+// Load routes
+const authRouter = require("./routes/auth.route");
+const userRouter = require("./routes/user.route");
+
+// Dev Logginf Middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(
+    cors({
+      origin: process.env.CLIENT_URL,
+    })
+  );
+  app.use(morgan("dev"));
+}
+
+// Use Routes
+app.post("/api/payments/create", async (request, response) => {
+  const total = request.query.total;
+
+  console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, // subunits of the currency
+    currency: "usd",
+    description: "Software development services",
+    shipping: {
+      name: "Jenny Rosen",
+      address: {
+        line1: "510 Townsend St",
+        postal_code: "98140",
+        city: "San Francisco",
+        state: "CA",
+        country: "US",
+      },
+    },
+  });
+
+  // OK - Created
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+app.use("/api", authRouter);
+app.use("/api", userRouter);
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    msg: "Page not founded",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
